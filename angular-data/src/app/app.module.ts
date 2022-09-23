876278879eac62ec94096f9bee4773866b10597b
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2022-09-23 08:55:58 +0000

    01.1

--- angular-data/src/app/app.module.ts
+++ angular-data/src/app/app.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { HelloComponent } from './hello/hello.component';
-import { NewComponent } from './new/new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent,
-    HelloComponent,
-    NewComponent
+    UserItemComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule
