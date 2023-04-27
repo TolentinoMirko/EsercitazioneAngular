3d3c9d2fc85631ba464c860bdddc7bc75db9f07a
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2023-04-27 12:45:13 +0000

    completato

--- angular-maps/src/app/app.component.ts
+++ angular-maps/src/app/app.component.ts
@@ -8,36 +8,51 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'server mappe';
 
-  center:any;
-  position:any;
-  label:string;
-  position1:any;
-  label1:string;
-  circleOptions :any;
+  center: any;
+  position: any;
+  label: string;
+  position1: any;
+  label1: string;
+  circleOptions: any;
   markerOptions: google.maps.MarkerOptions;
   vertices: google.maps.LatLngLiteral[];
+  triangleOptions:any;
 
-  constructor(){
-    this.center={lat: 45.506738, lng: 9.190766};
+  constructor() {
+    this.center = { lat: 45.506738, lng: 9.190766 };
     this.position = this.center;
     this.label = "ciao";
-    this.position1={lat:45.50165719082021,lng: 9.193760217371562};
+    this.position1 = { lat: 45.50165719082021, lng: 9.193760217371562 };
     this.label1 = "casa";
-    this.circleOptions = {fillColor : 'red'}
+    this.circleOptions = { fillColor: 'red' }
+    this.triangleOptions = {fillColor:'purple'}
     this.vertices = [
-      {  lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
-      {  lat: this.center.lat, lng: this.center.lng },
-      {  lat: this.center.lat - 0.001, lng: this.center.lng - 0.002}
- ];
+      { lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
+      { lat: this.center.lat, lng: this.center.lng },
+      { lat: this.center.lat - 0.001, lng: this.center.lng - 0.002 }
+    ];
 
-    let iconData : google.maps.Icon = {
-      url:'./assets/img/cat_acrobat.ico',
-      scaledSize : new google.maps.Size(60,60)
+    let iconData: google.maps.Icon = {
+      url: './assets/img/cat_acrobat.ico',
+      scaledSize: new google.maps.Size(60, 60)
 
-    
- }
-  
-this.markerOptions = {icon:iconData}
+
+    }
+
+    this.markerOptions = { icon: iconData }
   }
 
+  giallo() {
+    this.circleOptions = { fillColor: 'yellow' };
+    console.log("PROVA")
+  }
+
+  blu() {
+    this.circleOptions = { fillColor: 'blue' };
+    console.log("PROVA")
+  }
+  verde() {
+    this.triangleOptions = { fillColor: 'lime' };
+    console.log("PROVA")
+  }
 }
