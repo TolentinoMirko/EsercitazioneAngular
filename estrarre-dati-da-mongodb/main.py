3eff90d35c45dcf5078d70bffcee8da4cecb9667
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2023-05-18 12:56:17 +0000

    inizio 03. aggregazioni

--- estrarre-dati-da-mongodb/main.py
+++ estrarre-dati-da-mongodb/main.py
@@ -32,12 +32,66 @@ def get_all_addresses():
     return jsonify({'result': output})
 
 
-@app.route('/ci_vettore', methods=['GET'])
-def get_vettore():
+@app.route('/ci_vettore/<foglio>', methods=['GET'])
+def get_vettore(foglio):
+    foglio = int(foglio)
     mil4326WKT = mongo.db.MilWKT4326
     output = []
-    for s in mil4326WKT.find().limit(100):
-        output.append(s['CI_VETTORE'])
+    query = {"FOGLIO" : foglio}
+    for s in mil4326WKT.find(query).limit(100):
+        output.append({
+            "INDIRIZZO":s['INDIRIZZO'],
+            "WGS84_X":s["WGS84_X"],
+            "WGS84_Y":s["WGS84_Y"],
+            "CLASSE_ENE":s["CLASSE_ENE"],
+            "EP_H_ND":s["EP_H_ND"],
+            "FOGLIO":s["FOGLIO"],
+            "CI_VETTORE":s['CI_VETTORE'],
+            "SEZ":s['SEZ']})
+    return jsonify({'result': output})
+
+@app.route('/sezione/<sezione>', methods=['GET'])
+def get_sezione(sezione):
+    sezione = int(sezione)
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+
+    query = {"SEZ" : sezione}
+
+    for s in mil4326WKT.find(query).limit(100):
+        output.append({
+            "INDIRIZZO":s['INDIRIZZO'],
+            "WGS84_X":s["WGS84_X"],
+            "WGS84_Y":s["WGS84_Y"],
+            "CLASSE_ENE":s["CLASSE_ENE"],
+            "EP_H_ND":s["EP_H_ND"],
+            "FOGLIO":s["FOGLIO"],
+            "CI_VETTORE":s['CI_VETTORE']})
+    return jsonify({'result': output})
+
+@app.route('/geogeom', methods=['GET'])
+def get_all_stars():
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+
+    group = {
+        '$group': {
+            '_id': {
+                'SEZ': '$SEZ',
+                'WKT': '$WKT'
+            },
+            'AVG': {
+                '$avg': '$EP_H_ND'
+            },
+            'SUM': {
+                '$sum': '$EP_H_ND'
+            }
+        }
+    }
+
+    for s in mil4326WKT.aggregate([group]):
+        output.append({'somma': s['SUM'], 'media': s['AVG'],
+                      'WKT': s['_id']['WKT'], 'SEZ': s['_id']['SEZ']})
     return jsonify({'result': output})
 
 # Checks to see if the name of the package is the run as the main package.
