06b28e5079b062f9e5aa27a382496b44bac2c0e8
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2023-05-25 12:53:57 +0000

    pag 2 di 04.1 lab ,  mi da errore in implements AfterViewInit

--- RelabClient/angular/relabAngular/src/app/app.component.ts
+++ RelabClient/angular/relabAngular/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { AfterViewInit } from '@angular/core';
 import { Component, ViewChild } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps'
 import { Observable } from 'rxjs';
-import {  GeoFeatureCollection } from './models/geojson.model';
+import { GeoFeatureCollection } from './models/geojson.model';
+
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,9 @@ import {  GeoFeatureCollection } from './models/geojson.model';
 export class AppComponent implements AfterViewInit {
   title = 'server mappe';
   //Variabile che conterrà i nostri oggetti GeoJson
-  geoJsonObject : GeoFeatureCollection;
+  geoJsonObject! : GeoFeatureCollection;
   //Observable per richiedere al server python i dati sul DB
-  obsGeoData: Observable<GeoFeatureCollection>;
+  obsGeoData!: Observable<GeoFeatureCollection>;
   // Centriamo la mappa
   center: google.maps.LatLngLiteral = { lat: 45.506738, lng: 9.190766 };
   zoom = 8;
@@ -23,6 +24,7 @@ export class AppComponent implements AfterViewInit {
   constructor(public http: HttpClient) {
     //Facciamo iniettare il modulo HttpClient dal framework Angular (ricordati di importare la libreria)
   }
+
 
   //Metodo che scarica i dati nella variabile geoJsonObject
   prepareData = (data: GeoFeatureCollection) => {
@@ -33,7 +35,7 @@ export class AppComponent implements AfterViewInit {
  //Una volta che la pagina web è caricata, viene lanciato il metodo ngOnInit scarico i    dati 
   //dal server
   ngOnInit() {
-    this.obsGeoData = this.http.get<GeoFeatureCollection>("TUO_URL/ci_vettore/50");
+    this.obsGeoData = this.http.get<GeoFeatureCollection>("https://5000-tolentinomi-esercitazio-kbhb7zmknef.ws-eu97.gitpod.io//ci_vettore/50");
     this.obsGeoData.subscribe(this.prepareData);
   }
 }
