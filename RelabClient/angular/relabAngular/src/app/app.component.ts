be1a1eeeffcb4720047abefc95f050333f7ecdbb
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2023-05-29 09:45:19 +0000

    praticamente completato il 04.1 (senza es 1 e 2) non funziano i marker

--- RelabClient/angular/relabAngular/src/app/app.component.ts
+++ RelabClient/angular/relabAngular/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { AfterViewInit } from '@angular/core';
 import { Component, ViewChild } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps'
+import { GoogleMap } from '@angular/google-maps';
 import { Observable } from 'rxjs';
 import { GeoFeatureCollection } from './models/geojson.model';
-
+import { Ci_vettore } from './models/ci_vett.model';
 
 @Component({
   selector: 'app-root',
@@ -19,24 +19,62 @@ export class AppComponent implements AfterViewInit {
   obsGeoData!: Observable<GeoFeatureCollection>;
   // Centriamo la mappa
   center: google.maps.LatLngLiteral = { lat: 45.506738, lng: 9.190766 };
-  zoom = 8;
+  zoom = 16;
+  obsCiVett! : Observable<Ci_vettore[]>; //Crea un observable per ricevere i vettori energetici
+  markerList! : google.maps.MarkerOptions[];
+
+
 
   constructor(public http: HttpClient) {
     //Facciamo iniettare il modulo HttpClient dal framework Angular (ricordati di importare la libreria)
+    
   }
 
-
+  ngAfterViewInit(): void {
+    
+  }
+  
   //Metodo che scarica i dati nella variabile geoJsonObject
   prepareData = (data: GeoFeatureCollection) => {
     this.geoJsonObject = data
     console.log( this.geoJsonObject );
   }
 
+  prepareCiVettData = (data: Ci_vettore[]) =>
+  {
+    console.log(data); //Verifica di ricevere i vettori energetici
+    this.markerList = []; //NB: markers va dichiarata tra le proprietà markers : Marker[]
+    for (const iterator of data) { //Per ogni oggetto del vettore creo un Marker
+      let m : google.maps.MarkerOptions = 
+      {
+       position : new google.maps.LatLng (iterator.WGS84_X, iterator.WGS84_Y),
+       icon : this.findImage(iterator.CI_VETTORE),
+       label : iterator.CI_VETTORE 
+      }
+      //Marker(iterator.WGS84_X,iterator.WGS84_Y,iterator.CI_VETTORE);
+      this.markerList.push(m);
+  }
+}
+findImage(label: string) : google.maps.Icon {
+  if (label.includes("Gas")) {
+    return { url: './assets/img/gas.ico', scaledSize: new google.maps.Size(32,32) };
+  }
+  if (label.includes("elettrica")) {
+    return { url: './assets/img/electricity.ico', scaledSize: new google.maps.Size(32,32) };
+  }
+  //Se non viene riconosciuta nessuna etichetta ritorna l'icona undefined
+    return {url: './assets/img/undefined.ico' , scaledSize: new google.maps.Size(32,32)}
+}
+
+
  //Una volta che la pagina web è caricata, viene lanciato il metodo ngOnInit scarico i    dati 
   //dal server
   ngOnInit() {
-    this.obsGeoData = this.http.get<GeoFeatureCollection>("https://5000-tolentinomi-esercitazio-kbhb7zmknef.ws-eu97.gitpod.io//ci_vettore/50");
+    this.obsGeoData = this.http.get<GeoFeatureCollection>("https://5000-tolentinomi-esercitazio-bs2zyv1u4ow.ws-eu98.gitpod.io/ci_vettore/50");
     this.obsGeoData.subscribe(this.prepareData);
   }
+
 }
 
+
+  
