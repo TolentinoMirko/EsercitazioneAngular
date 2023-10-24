ff8be1f603ca8e31974a3ddb8375626b98199dc5
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2023-10-24 09:45:45 +0000

    inizio ripasso-es-debito

---
+++ ripasso-es-debito/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PokemonType } from './type.module';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ripasso-es-debito';
+  obs!: Observable<Object>;
+  data : Object = undefined!;
+  constructor(private http:HttpClient){
+    this.obs= this.http.get<PokemonType[]>("https://pokeapi.co/api/v2/type")
+    this.obs.subscribe(this.doSomething)
+  }
+
+  doSomething = (data : PokemonType) => {
+    this.data = data
+  }
+}
---
+++ ripasso-es-debito/src/app/type.module.ts
@@ -0,0 +1,12 @@
+export interface PokemonType {
+    count: number
+    next: any
+    previous: any
+    results: Result[]
+  }
+  
+  export interface Result {
+    name: string
+    url: string
+  }
+  
