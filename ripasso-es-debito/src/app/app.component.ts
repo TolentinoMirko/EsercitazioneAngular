0b6afe408b2865e052e2a40e629a26d738d1bbec
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2023-10-26 08:57:23 +0000

    manca trovare i pokemon di quel tipo

--- ripasso-es-debito/src/app/app.component.ts
+++ ripasso-es-debito/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   title = 'ripasso-es-debito';
   obs: Observable <PokemonType> ;
   data!: PokemonType;
+  url!:any;
   constructor(private http : HttpClient){
     this.obs = this.http.get<PokemonType>("https://pokeapi.co/api/v2/type")
     this.obs.subscribe(this.doSomething)
@@ -19,5 +20,9 @@ export class AppComponent {
 
   doSomething = (data : PokemonType) => {
     this.data = data
+  }
+
+  getLastPart = (url : any) => {
+    this.url.split('/').pop()
   }
 }
